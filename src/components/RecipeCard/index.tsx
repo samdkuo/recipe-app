d3ed3680fd5b4171d5f75d637de57debe41bbeb9
samdkuo <samanthakuo97@gmail.com>
2021-11-29 11:13:55 -0800

    styles to recipe card

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import { colors } from "../../theme/colors";
 
 interface RecipeCardProps {
@@ -10,29 +10,33 @@ interface RecipeCardProps {
 
 function RecipeCard({ title, totalIngredients, cookTime }: RecipeCardProps) {
   return (
-    <TouchableOpacity
-      style={{
-        borderWidth: 1,
-        borderRadius: 8,
-        borderColor: "#d9dddc",
-        overflow: "hidden",
-        width: 250,
-        backgroundColor: colors.gray050,
-      }}
-    >
-      <Image
-        style={{ width: "100%", height: 200 }}
-        source={{
-          uri: "https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+    <Pressable>
+      <View
+        style={{
+          borderRadius: 8,
+          shadowColor: colors.gray400,
+          shadowOffset: { width: 0, height: 1 },
+          shadowOpacity: 0.3,
+          shadowRadius: 8,
+          overflow: "hidden",
+          width: 250,
+          backgroundColor: colors.gray050,
         }}
-      />
-      <View style={{ padding: 8 }}>
-        <Text>{title}</Text>
-        <View>
-          <Text>{`${totalIngredients} ingredients | ${cookTime} min`}</Text>
+      >
+        <Image
+          style={{ width: "100%", height: 200 }}
+          source={{
+            uri: "https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
+          }}
+        />
+        <View style={{ padding: 8, backgroundColor: "#FFFFFA" }}>
+          <Text>{title}</Text>
+          <View>
+            <Text>{`${totalIngredients} ingredients | ${cookTime} min`}</Text>
+          </View>
         </View>
       </View>
-    </TouchableOpacity>
+    </Pressable>
   );
 }
 
