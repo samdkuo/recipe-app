b706b1667bdc86bbe29b6850bb1196fb4fa3fb49
samdkuo <samanthakuo97@gmail.com>
2021-12-02 09:21:45 -0800

    feat: add colorBrightness to color

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -83,3 +83,22 @@ export const colors = {
   deepOrange: isLightMode ? "#EE7850" : "#FF8D66",
   brown: isLightMode ? "#886F65" : "#B29285",
 };
+
+export function colorBrightness(hex: string, lum: number): string {
+  hex = String(hex).replace(/[^0-9a-f]/gi, "");
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  lum = lum || 0;
+
+  let rgb = "#";
+  let c;
+  let i;
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i * 2, 2), 16);
+    c = Math.round(Math.min(Math.max(0, c + c * lum), 255)).toString(16);
+    rgb += ("00" + c).substr(c.length);
+  }
+
+  return rgb;
+}
