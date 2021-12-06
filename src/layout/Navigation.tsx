e74644c4de5b20d887aca458fd23561c8f0d9032
samdkuo <samanthakuo97@gmail.com>
2021-12-06 11:32:38 -0800

    feat: use link component for navigation

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Image } from "react-native";
-// import { Link } from "react-router-dom";
+import { Link as ReactLink } from "react-router-dom";
 
 import { Typography, Link } from "../components";
 import { colors } from "../theme/colors";
@@ -18,13 +18,13 @@ const Navigation = ({ routes }: { routes: any }) => {
         marginBottom: 16,
       }}
     >
-      {/* <Link to="/">
+      <ReactLink to="/">
         <Image
           source={{ uri: "images/letsEatTransparent.png" }}
           resizeMode="cover"
           style={{ width: 90, height: 90 }}
         />
-      </Link> */}
+      </ReactLink>
       <View
         style={{
           flex: 0.5,
@@ -35,25 +35,10 @@ const Navigation = ({ routes }: { routes: any }) => {
       >
         {routes.map((route: any, index: number) => {
           if (route.path !== "/") {
-            return (
-              <Link link={route.path} />
-              // <Link to={route.path} style={{ textDecoration: "none" }}>
-              //   <Typography
-              //     key={index}
-              //     // font="600"
-              //     style={{
-              //       fontSize: 16,
-              //       marginLeft: 16,
-              //       // textShadowColor: colors.tertiary,
-              //       // textShadowOffset: { width: 2, height: 2 },
-              //       // textShadowRadius: 2,
-              //     }}
-              //   >
-              //     {route.path.substring(1, 2).toUpperCase()}
-              //     {route.path.substring(2).toLowerCase()}
-              //   </Typography>
-              // </Link>
-            );
+            const nav =
+              route.path.substring(1, 2).toUpperCase() +
+              route.path.substring(2).toLowerCase();
+            return <Link key={index} link={nav} />;
           }
         })}
       </View>
