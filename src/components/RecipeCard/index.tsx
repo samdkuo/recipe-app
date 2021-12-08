fa349fbbb453ed705e7634266e4ec2fc3ac465eb
samdkuo <samanthakuo97@gmail.com>
2021-12-08 09:37:15 -0800

    feat: update card size

--- src/components/RecipeCard/index.tsx
+++ src/components/RecipeCard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Image, Pressable } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
 import { Typography } from "..";
 import { colors } from "../../theme/colors";
 
@@ -11,33 +11,31 @@ interface RecipeCardProps {
 
 function RecipeCard({ title, totalIngredients, cookTime }: RecipeCardProps) {
   return (
-    <Pressable>
-      <View
-        style={{
-          borderRadius: 4,
-          shadowColor: colors.gray400,
-          shadowOffset: { width: 0, height: 1 },
-          shadowOpacity: 0.3,
-          shadowRadius: 8,
-          overflow: "hidden",
-          width: 250,
-          backgroundColor: colors.gray050,
+    <TouchableOpacity
+      style={{
+        borderRadius: 4,
+        shadowColor: colors.gray400,
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.3,
+        shadowRadius: 8,
+        overflow: "hidden",
+        width: 250,
+        backgroundColor: colors.gray050,
+      }}
+    >
+      <Image
+        style={{ width: "100%", height: 200 }}
+        source={{
+          uri: "https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
         }}
-      >
-        <Image
-          style={{ width: "100%", height: 200 }}
-          source={{
-            uri: "https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80",
-          }}
-        />
-        <View style={{ padding: 16 }}>
-          <Typography font="500">{title}</Typography>
-          <View>
-            <Typography>{`${totalIngredients} ingredients | ${cookTime} min`}</Typography>
-          </View>
+      />
+      <View style={{ padding: 16 }}>
+        <Typography font="500">{title}</Typography>
+        <View>
+          <Typography>{`${totalIngredients} ingredients | ${cookTime} min`}</Typography>
         </View>
       </View>
-    </Pressable>
+    </TouchableOpacity>
   );
 }
 
