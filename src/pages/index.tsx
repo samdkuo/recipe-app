63291bffefd10061a60172824d0e89d62ab94737
samdkuo <samanthakuo97@gmail.com>
2022-01-10 11:00:29 -0800

    feat: add category pills

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Image, View } from "react-native";
 import Layout from "../layout/Layout";
-import { Typography } from "../components/index";
+import { Pill, Typography } from "../components/index";
 import { Search } from "semantic-ui-react";
 
 const Home = () => {
   return (
     <View>
       <Search />
+      <View>
+        {[
+          "Breakfast",
+          "Entrees",
+          "Bread",
+          "Side dishes",
+          "Dessert",
+          "Drinks",
+        ].map((category, index) => (
+          <Pill>{category}</Pill>
+        ))}
+      </View>
     </View>
   );
 };
