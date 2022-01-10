2b80df74cb280ef7568ccb7b506834e850c66196
samdkuo <samanthakuo97@gmail.com>
2022-01-10 15:28:38 -0800

    feat: add image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,11 @@ import { Pill, Typography, Grid, TextInput } from "../components/index";
 const Home = () => {
   return (
     <View>
+      <Image
+        source={{ uri: "images/letsEatTransparent.png" }}
+        resizeMode="cover"
+        style={{ width: 400, height: 400 }}
+      />
       <TextInput placeholder="Search..." />
       <Grid numColumns={6} gap={16}>
         {[
